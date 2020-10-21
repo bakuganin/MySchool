b1a5e19884f53df3034baec12d22bd47d9f9b802
Bakugan <kafder97@gmail.com>
2020-10-21 10:36:19 +0300

    start

---
+++ src/sptvr19/myschool/App.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19.myschool;
+
+/**
+ *
+ * @author user
+ */
+class App {
+
+    void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
